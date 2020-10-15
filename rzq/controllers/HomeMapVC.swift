6a47191187f203e2faf9219043d0868f553b2254
elmansour said <said.elmansour@trutify.com>
2020-10-15 11:33:37 +0100

    Fix Zoom out for driver location & search bar placeholder

--- rzq/controllers/HomeMapVC.swift
+++ rzq/controllers/HomeMapVC.swift
@@ -602,7 +602,7 @@ class HomeMapVC: BaseViewController {
         if fromLatitude ?? 0.0 > 0.0 && fromLongitude ?? 0.0 > 0.0 {
             bounds = bounds.includingCoordinate(self.pickMarker?.position ?? CLLocationCoordinate2D(latitude: self.latitude ?? 0.0, longitude: self.longitude ?? 0.0))
            // bounds = bounds.includingCoordinate(self.dropMarker?.position ?? CLLocationCoordinate2D(latitude: self.latitude ?? 0.0, longitude: self.longitude ?? 0.0))
-            let camera : GMSCameraPosition = GMSCameraPosition.camera(withLatitude: fromLatitude ?? 0.0, longitude: longitude ?? 0.0, zoom: 12, bearing: 3, viewingAngle: 0)
+            let camera : GMSCameraPosition = GMSCameraPosition.camera(withLatitude: fromLatitude ?? 0.0, longitude: longitude ?? 0.0, zoom: 15, bearing: 3, viewingAngle: 0)
             
             mapView.camera = camera
         } else {
