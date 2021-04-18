c3cfafbcedc75629241aeeb20af8ad424b4d7a2b
elmansour said <said.elmansour@ibm.com>
2021-04-18 03:07:51 +0000

    Fix the crash of the Realm

--- rzq/structure/AppDelegate.swift
+++ rzq/structure/AppDelegate.swift
@@ -91,15 +91,25 @@ class AppDelegate: UIResponder, UIApplicationDelegate, MOLHResetable,MessagingDe
         AFManager = Alamofire.SessionManager(configuration: configuration)
         
         
-        var realmConfig = Realm.Configuration(
-            schemaVersion: 1,
+        let config = Realm.Configuration(
+            // Set the new schema version. This must be greater than the previously used
+            // version (if you've never set a schema version before, the version is 0).
+            schemaVersion: 4,
+
+            // Set the block which will be called automatically when opening a Realm with
+            // a schema version lower than the one set above
             migrationBlock: { migration, oldSchemaVersion in
-                if oldSchemaVersion < 1 {
+                // We haven’t migrated anything yet, so oldSchemaVersion == 0
+                if (oldSchemaVersion < 100000000000) {
                 }
-        }
-        )
-        
-        Realm.Configuration.defaultConfiguration = realmConfig
+            })
+
+        // Tell Realm to use this new configuration object for the default Realm
+        Realm.Configuration.defaultConfiguration = config
+
+        // Now that we've told Realm how to handle the schema change, opening the file
+        // will automatically perform the migration
+        var realm = try! Realm()
         
         Fabric.with([Crashlytics.self])
         
